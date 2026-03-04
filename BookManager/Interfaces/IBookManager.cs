b0d9d9a8b3db3d9ad512dd75077879c68d5dfa10
oleksandr-penia <penya.s@ukr.net>
2026-03-04 23:23:03 +0200

    Update IBookManager interface

--- BookManager/Interfaces/IBookManager.cs
+++ BookManager/Interfaces/IBookManager.cs
@@ -1,6 +1,14 @@
-﻿namespace BookManager.Interfaces
+﻿using BookManager.Models;
+
+namespace BookManager.Interfaces
 {
     public interface IBookManager
     {
+        IEnumerable<Book> GetBooks();
+        void Load(string path, string rootElement);
+        void Add(Book book);
+        void Sort(IComparer<Book> comparer);
+        IEnumerable<Book> SearchByTitle(string search);
+        void Save(string path, string rootElement);
     }
 }
